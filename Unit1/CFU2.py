efef675d5dfcba51ff5f5fa5d4be69d61ece5370
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-09-16 12:10:53 -0400

    Create CFU2.py

---
+++ Unit1/CFU2.py
@@ -0,0 +1,6 @@
+firstName = input("What is your first name?")
+lastName = input("What is your last name?")
+age = input("How old are you?")
+time = input("What is the longest amount of time you have gone without sleep? (in hours)")
+since = input("How long (in years) is it till you turn 21 or how long has it been since you were 21?")
+print("Once upon a time " + firstName + " " + lastName + ", who was only " + age + " at the time, \ndecided to start playing video games one afternoon. \n" + firstName + " " + lastName + " then kept playing for " + time + ". \nUntil at one moment in time they had finished playing and those " + time + " are lost and never to be regained. \nBut maybe they can put the time they have in the next " + since + " to better use.")
