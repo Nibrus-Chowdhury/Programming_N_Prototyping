4a33cc9deff19ec347232c1bbc6c883866280a1f
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-09-19 19:15:37 -0400

    Update groupChallenge1.py

--- Unit1/groupChallenge1.py
+++ Unit1/groupChallenge1.py
@@ -12,16 +12,17 @@ celcius = (faren-32)*(5/9)
 print(faren,"°F is",celcius,"°C")
 
 #Challenge 3
-print("\nChallenge #2:\n")
+print("\nChallenge #3:\n")
 initVertVelo = int(input("Give me the initial vertical velocity of the object"))
 time = int(input("How long did this object go up for?"))
 vertDistance = (initVertVelo*time)-(1/2)*9.8*time**2
 print("The vertical distance of the projectile is",vertDistance,"meters")
 
 #Bonus Challenge:
+print("\nBonus Challenge:\n")
 xCoor1 = int(input("Give me the x coordinate of one point"))
 yCoor1 = int(input("Give me the y coordinate of one point"))
 xCoor2 = int(input("Give me the x coordinate of another point"))
 yCoor2 = int(input("Give me the y coordinate of another point"))
 distance = ((xCoor2-xCoor1)**2+(yCoor2-yCoor1)**2)**0.5
-print("The distance between these two points is",distance)
+print("The distance between these two points is",distance,"units")
