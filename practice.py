4adbb984a24154caaa99d3be5fa29c2c0730da2f
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-09-23 12:08:19 -0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -3,3 +3,7 @@ r = int(input("What is the radius of your circle?"))
 C = 2*r*math.pi
 A = math.pi*r**2
 print("C=",C," A=",A)
+rCyl = int(input("What is the radius of the cylinder?"))
+h = int(input("What is the height of the cylinder?"))
+v = rCyl**2*math.pi*h
+print("V=",v)
