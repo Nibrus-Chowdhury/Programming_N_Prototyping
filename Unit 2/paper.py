7e9bd5dd69cc99697bd04796e6500744cb9e2d5d
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-09-27 12:48:47 -0400

    Create paper.py

---
+++ Unit 2/paper.py
@@ -0,0 +1,7 @@
+p = float(input("How many pennies do you have?"))*0.01
+c = float(input("How many nickels do you have?"))*0.05
+d = float(input("How many dimes do you have?"))*0.1
+q = float(input("How many quarters do you have?"))*0.25
+l = float(input("How many loonies do you have?"))
+t = float(input("How many toonies do you have?"))*2
+print("The total amount of money is $"+str(p+n+d+q+l+t))
