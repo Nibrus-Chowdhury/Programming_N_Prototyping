09433be3383956a3cec975022f7b233e786c4aaf
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-10-10 12:35:44 -0400

    Update practice.py

--- Unit 2/practice.py
+++ Unit 2/practice.py
@@ -1,3 +1,4 @@
+# Math
 import math
 r = int(input("What is the radius of your circle?"))
 C = 2*r*math.pi
@@ -7,3 +8,10 @@ rCyl = int(input("What is the radius of the cylinder?"))
 h = int(input("What is the height of the cylinder?"))
 v = rCyl**2*math.pi*h
 print("V=",int(v))
+
+# Conditionals
+x = int(input("Give me a number: "))
+if x >= 5:
+    print("Greater than or equal to 5!")
+else:
+    print("This number is less than 5")
