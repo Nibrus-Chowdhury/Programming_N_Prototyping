d269097695843d775c42bcf669f2df3714afdb69
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-10-15 11:34:54 -0400

    Delete Unit 2/CFU#7.py

--- Unit 2/CFU#7.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
-num3 = int(input("Enter a number: "))
-def addNumbers():
-    print("The sum of your three numbers is:",num1+num2+num3)
-addNumbers()
-def averageNumbers():
-    print("The average of your three numbers is:",(num1+num2+num3)/3)
-averageNumbers()
