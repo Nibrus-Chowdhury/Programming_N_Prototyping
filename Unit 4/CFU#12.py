eaa395313a12da1818cdeb7606e9c4451926ab1c
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-10-29 12:45:58 -0400

    Update CFU#12.py

--- Unit 4/CFU#12.py
+++ Unit 4/CFU#12.py
@@ -2,12 +2,18 @@
 Nibrus Chowdhury
 10/29/24
 Period 5-6
-Description: Asks the user for a password and if the password is not correct then it asks again until the user gets the password.
+Description: Asks the user for a password and if the password is not correct then it asks again until the user gets the password. Only 3 Chances.
 """
 
 password = input("Enter Password: ")
-while password != "simonsnyc":
-    print("Wrong Password!")
-    password = input("Enter Password: ")
-print("Correct! You may enter...")
-    
+chance = 0
+while chance != 3:
+        if password == "simonsnyc":
+            print("Correct! You may enter...")
+            chance = 3
+        else:
+            print("Wrong Password!")
+            password = input("Enter Password: ")
+            chance += 1
+            if chance == 3:
+                print("Too many chances.")
