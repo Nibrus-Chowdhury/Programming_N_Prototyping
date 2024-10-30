7dd6769b7c2501818ac98256ecb2acc195a1f705
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-10-30 11:13:52 -0400

    Update CFU#12.py

--- Unit 4/CFU#12.py
+++ Unit 4/CFU#12.py
@@ -5,21 +5,31 @@ Period 5-6
 Description: Asks the user for a password and if the password is not correct then it asks again until the user gets the password. Only 3 Chances.
 """
 
-def check(password):
-    correct = "simonsnyc"
-    if password == correct:
-        print("Correct! You may enter...")
-    else:
+option = int(input("Which version do you want to play? (Enter 1 or 2)"))
+
+if option == 1:
+    password = "simonsnyc"
+    user = input("Enter password: ")
+    while user != "simonsnyc":
         print("Wrong Password!")
-def attempts():
-    attempts = 0
-    while attempts < 3:
-        password = input("Enter the password: ")
-        check(password)
-        if password == "simonsnyc":
-            attempts = 3 
+        user = input("Enter passowrd: ")
+    print("Correct! You may enter...")
+else:
+    def check(password1):
+        correct = "simonsnyc"
+        if password1 == correct:
+            print("Correct! You may enter...")
         else:
-            attempts += 1
-        if attempts == 3 and password != "simonsnyc":
-            print("Access denied. No more attempts left.")
-attempts()
+            print("Wrong Password!")
+    def attempts():
+        attempts = 0
+        while attempts < 3:
+            password1 = input("Enter the password: ")
+            check(password1)
+            if password1 == "simonsnyc":
+                attempts = 3 
+            else:
+                attempts += 1
+            if attempts == 3 and password1 != "simonsnyc":
+                print("Access denied. No more attempts left.")
+    attempts()
