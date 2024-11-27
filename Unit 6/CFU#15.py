806671e187ab5f91289b11ae19d99f171b465e1f
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-11-26 21:39:32 -0500

    Update CFU#15.py

--- Unit 6/CFU#15.py
+++ Unit 6/CFU#15.py
@@ -9,13 +9,13 @@ import simplegui
 
 def draw_handler(canvas):
     # all drawing happens here
-    # canvas.draw_point(x,y)
-    canvas.draw_point([100,100], "black")
-    # canvas.draw_line([x1,y2],[x2,y2], line_width, line_color)
-    canvas.draw_line([40,50],[50,50], 20, "black")
     #canvas.draw_polygon([(x1,y1),(x2,y2),(x3,y3)], line_width, color)
     canvas.draw_polygon([(50,50),(50,150),(150,150),(150,50)], 5, "yellow")
-    canvas.draw_polygon([(85,75),()], 5, "yellow")
+    canvas.draw_polygon([(80,75),(80,77),(82,77),(82,75)], 5, "yellow")
+    canvas.draw_polygon([(120,75),(120,77),(122,77),(122,75)], 5, "yellow")
+    canvas.draw_polygon([(101,90),(101,105)], 1, "yellow")
+    canvas.draw_polygon([(70,125),(80,135),(127,135),(137,125)], 1, "yellow")
+    
     
 #library.create_frame(title, width, height)
 frame = simplegui.create_frame("CFU 15 Happy Shapes",200,200)
