71cf2f43899492cbfd2693f834dfad9ec1882d66
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-12-19 12:45:53 -0500

    Create lists.py

---
+++ Unit 7/lists.py
@@ -0,0 +1,6 @@
+people=["Marisa","Hailey","Arafath","Neelam","Harbans","Grace","Farhan","Sharena","Edwin","Sarika","Amelia","Evelyn","Angel","Romina","Malaci","Syeda","Nafisa","Emirah","Sheldon"]
+donation=["Cookies","Sugar Cookies and Cupcakes","Chips","Cookies","Scooby Snacks","Sour Patch","Nothing","Utensils, Plates, Cups","Soda","Pretzels","Candy","Donuts","Chips","Chips","Drinks","Choc Chip Cookies","Cookies","Food","Chips"]
+#for person in people:
+#    print("This is my friend " + person)
+for i in range(19):
+    print(f"{people[i]} will bring {donation[i]}.")
